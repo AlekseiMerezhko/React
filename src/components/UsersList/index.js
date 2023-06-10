00a6c5de0a30864325bc55ea427395aea15b75e9
Oleksii Merezhko <merezhko.aleksei@gmail.com>
2023-06-10 13:06:15 +0300

    commit-2

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,31 +1,58 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import Input from "../../baseComponents/Input";
 
 class UsersList extends Component {
-    state = { users: [], value: "" };
+    state = { value: "", renderInput: true };
     handleAddUser = () => {
         this.setState({ users: [this.state.value] });
     };
     handleChange = (e) => {
-        console.log("event", e.target.value);
+        // console.log("event", e.target.value);
         this.setState({ value: e.target.value });
     };
+    handleInputToggle = () => {
+        this.setState({ renderInput: false });
+    };
+    renderUsers = () => {
+        if (Array.isArray(this.props.users)) {
+            return this.props.users.map((user) => (
+                <div key={user.name}>{user.name}</div>
+            ));
+        }
+    };
     render() {
-        console.log("this.state", this.state);
+        console.log(this.props.users);
         return (
             <div>
-                {this.state.users.map((user) => (
-                    <div key={user}>{user}</div>
-                ))}
-                
-                <Input
-                    value={this.state.value}
-                    handleChange={this.handleChange}
-                />
-                <button onClick={this.handleAddUser}>Button</button>
+                {this.renderUsers()}
+
+                {this.state.renderInput && (
+                    <Input
+                        value={this.state.value}
+                        handleChange={this.handleChange}
+                    />
+
+                )}
+
+                <button onClick={this.handleInputToggle}>Button</button>
             </div>
         );
     }
 }
+
+UsersList.propTypes = {
+    users: PropTypes.arrayOf(PropTypes.shape),
+};
+
+UsersList.defaultProps = {
+    users: [
+        {
+            name: "Rahul",
+            eyeColor: "deepblue",
+            age: "45",
+        },
+    ],
+};
 
 export default UsersList;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <div>HomeHomeHomeHomeHomeHome</div>;
+};
+
+export default Home;
